16d226c8aebca0a3dd04b7a89390b2b6eb88dd1b
Amadeusz Wieczorek <git@amadeusw.com>
2024-02-03 21:43:41 -0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,29 @@
 use std::{fs};
-use std::io::{Error, ErrorKind};
+use std::io::{BufRead, BufReader, Error, ErrorKind, Write};
 use uuid::Uuid;
 use std::path::{Path, PathBuf};
 use std::collections::HashMap;
 
 const DATADIR: &str = ".taskspace";
+const INDEXFILE: &str = "index.txt";
 
 fn main() {
     println!("Hello, world!");
 }
 
-fn getDataDirectory() -> Result<PathBuf, Error> {
+fn getDataDirectory() -> Option<PathBuf> {
     let currentDir = std::env::current_dir()
-        .expect("Can't find current directory");
+        .expect("Expected to find the current directory");
     return findPathInTree(currentDir, DATADIR);
 }
 
-fn findPathInTree(startDir: PathBuf, targetDir: &str) -> Result<PathBuf, Error> {
+fn findPathInTree(startDir: PathBuf, targetDir: &str) -> Option<PathBuf> {
     loop {
         let mut currentPath = startDir;
         // Check if the target path is found
         let candidatePath = currentPath.join(DATADIR);
         if candidatePath.exists() {
-            return Ok(candidatePath);
+            return Some(candidatePath);
         }
 
         // Move up to the parent directory
@@ -33,19 +34,43 @@ fn findPathInTree(startDir: PathBuf, targetDir: &str) -> Result<PathBuf, Error> 
     }
 
     // If the target path is not found in the entire directory tree
-    return Err(Error::new(ErrorKind::NotFound, "Data directory not found"));
+    None
 }
 
 fn createTask(alias: &str, dataDir: &PathBuf) -> Result<(), Error> {
-    // Generate guid
     let id = Uuid::new_v4();
-    // Create folder
-    appendToIndex(alias, id, dataDir);
+
+    let taskPath = dataDir.join(format!("{}", id));
+    fs::create_dir_all(&taskPath);
+
+    appendToIndex(alias, &id, dataDir);
     return Ok(());
 }
 
 fn appendToIndex(alias: &str, id: &uuid::Uuid, dataDir: &PathBuf) -> Result<(), Error> {
+    let indexPath = dataDir.join(INDEXFILE);
+    let mut file = fs::File::create(indexPath)
+        .expect(&format!("Expected to access the index file {}", indexPath.display()));
+    //write!("{}:{id}", alias, id);
     return Ok(());
+}
+
+fn getIdFromIndex(alias: &str, dataDir: &PathBuf) -> Result<&uuid::Uuid, Error> {
+    let indexPath = dataDir.join(INDEXFILE);
+    let file = fs::File::open(indexPath)
+        .expect(&format!("Expected to open the index file {}", indexPath.display()));
+    let reader = BufReader::new(file);
+    
+    // Iterate over lines in the file
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+    return Ok(());
+}
+
+fn getOrCreateIndex(dataDir: &PathBuf) -> () {
+    // Proposal: instead of passing dataDir to other methods,
+    // get index and use functions on the index class
 }
 
 fn init() -> Result<(), Error> {
